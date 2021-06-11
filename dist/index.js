b3ee62f199d25751cac3fe16e89ced50e50c9598
Lessley Dennington <ledennin@microsoft.com>
2021-06-11 11:51:01 -0700

    Testing updates

--- dist/index.js
+++ dist/index.js
@@ -8356,7 +8356,7 @@ function run() {
             manifestText = manifestText.replace('{{url}}', fullUrl);
             core.debug('setting version...');
             // winget requires we remove the 'vfs' characters from the microsoft/git manifest version to align with the version shown in Control Panel
-            if (`${process.env.GITHUB_REPOSITORY}` === 'microsoft/git') {
+            if (`${process.env.GITHUB_REPOSITORY}` === 'ldennington/git') {
                 version.removeChars();
             }
             manifestText = manifestText.replace('{{version}}', version.toString());
